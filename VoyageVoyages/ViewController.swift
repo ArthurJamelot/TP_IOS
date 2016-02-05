7014718c60d0e92f4dd9a7992fced45ad538d022
arthur jamelot <arthur.jamelot@igtp3-mac09.isim.intra>
2016-02-05 13:52:48 +0100

    Initial Commit

---
+++ VoyageVoyages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoyageVoyages
+//
+//  Created by arthur jamelot on 05/02/2016.
+//  Copyright © 2016 arthur jamelot et charles bordier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
